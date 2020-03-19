eb5013b1bf907a2c9e0cc64f69d08827e8c3c78b
Zeta <38230184+Zeta7@users.noreply.github.com>
2020-03-19 14:05:21 -0600

    First Commit

---
+++ NONOGRAM/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NONOGRAM
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            READER reader = new READER("C:/Users/Zeta/Documents/TestFile.txt");
+            String[] Da = reader.GetData();
+            String j = Da[0];
+            String k = Da[1];
+            Console.WriteLine(j);
+            Console.WriteLine(k);
+            Console.WriteLine();
+            //Nonogram.Solve(j,k);
+            Console.ReadLine();
+        }
+        
+        
+
+    }
+}
+
