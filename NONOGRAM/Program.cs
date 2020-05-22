67633439573185cd5a53e6ae1303dfa6b4332f7c
Zeta <38230184+Zeta7@users.noreply.github.com>
2020-05-21 22:37:15 -0600

    Fix reader erros
    Fix error on reading files, add test files

--- NONOGRAM/Program.cs
+++ NONOGRAM/Program.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
 
 namespace NONOGRAM
 {
@@ -11,12 +12,35 @@ namespace NONOGRAM
 
         static void Main()
         {
-            READER reader = new READER("C:/Users/Zeta/Documents/TestFile.txt");
-            String[] Da = reader.GetData();
-            Console.WriteLine(Da[0]);
-            Console.WriteLine(Da[1]);
-            Console.WriteLine();
-            Nonogram.Solve(Da[0],Da[1]);
+
+
+            READER reader = new READER("");//In this part you need paste the path file, the files are on directory "Test Files"
+            List<List<String>> Da = reader.GetData();
+            string x = "";
+            for (int i = 0; i < Da[0].Count; i++)
+            {
+                x = x + Da[0][i];
+                if (i < Da[0].Count-1)
+                {
+                    x = x + ' ';
+                }
+            }
+            string y = "";
+            for (int i = 0; i < Da[1].Count; i++)
+            {
+                y = y + Da[1][i];
+                if (i < Da[1].Count - 1)
+                {
+                    y = y + ' ';
+                }
+            }
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Stopwatch watch = new Stopwatch();
+             watch.Start();
+            Nonogram.Solve(x, y);
+            watch.Stop();
+             Console.WriteLine(watch.Elapsed);
             Console.ReadLine();
         }
         
